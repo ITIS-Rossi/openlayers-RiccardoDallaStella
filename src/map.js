b6f77c91cd84ce53a75a7f543fad0d3a35c123eb
RiccardoDallaStella <9698272@itisrossi.vi.it>
2023-02-13 08:23:14 +0100

    Aggiunto Layer per Popup e creazione mappa

--- src/map.js
+++ src/map.js
@@ -1,4 +1,40 @@
 // Funzione init caricata all'avvio ...
 
 function init() {
+  const container = document.getElementById('popup');
+  const content = document.getElementById('popup-content');
+  const closer = document.getElementById('popup-closer');
+
+  const overlay = new ol.Overlay({
+    element: container,
+    autoPan: {
+      animation: {
+        duration: 250,
+      },
+    },
+  });
+  
+  ol.proj.useGeographic();
+  let map = new ol.Map({
+    target: "mappa",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: [11.555601582952226, 45.552535373028576],
+      zoom: 17,
+    }),
+  });
+  var layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        features: [
+            new ol.Feature({
+                geometry: new ol.geom.Point([11.555601582952226, 45.552535373028576])
+            })
+        ]
+    })
+  });
+  map.addLayer(layer);
 }
