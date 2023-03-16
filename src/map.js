6625a3f68b776ace49bc7f9dbe1e09e846b17f09
RiccardoDallaStella <9698272@itisrossi.vi.it>
2023-03-16 12:22:47 +0100

    Sistemazione generale

--- src/map.js
+++ src/map.js
@@ -1,15 +1,14 @@
 // Funzione init caricata all'avvio ...
 
 function init() {
-  const container = document.getElementById("popup");
-  const content = document.getElementById("popup-content");
-  const pos1 = [11.555466748422223, 45.55268745];
-  const pos2 = [11.553702, 45.552662];
+  const popup = document.getElementById("popup");
+  const posRossi = [11.555466, 45.552687];
+  const posRotatoria = [11.553702, 45.552662];
 
   ol.proj.useGeographic();
   
   const overlay = new ol.Overlay({
-    element: container,
+    element: popup,
     autoPan: {
       animation: {
         duration: 250,
@@ -18,32 +17,28 @@ function init() {
   });
   
   const featureRossi = new ol.Feature({
-    geometry: new ol.geom.Point(pos1),
+    geometry: new ol.geom.Point(posRossi),
     name: "ITIS Rossi"
   })
 
-  const iconStyle1 = new ol.style.Style({
-    image: new ol.style.Icon({
-      anchor: [0.5, 1],
-      src: "img/marker.png"
-    }),
-  });
-  
-  featureRossi.setStyle(iconStyle1);
-
   const featureRotatoria = new ol.Feature({
-    geometry: new ol.geom.Point(pos2),
+    geometry: new ol.geom.Point(posRotatoria),
     name: "Rotatoria del Rossi"
   })
 
-  const iconStyle2 = new ol.style.Style({
+  const iconStyle = new ol.style.Style({
     image: new ol.style.Icon({
       anchor: [0.5, 1],
       src: "img/marker.png"
     }),
   });
-  
-  featureRotatoria.setStyle(iconStyle2);
+
+  const layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: [featureRossi, featureRotatoria]
+    }),
+    style: iconStyle,
+  });
 
   const map = new ol.Map({
     target: "mappa",
@@ -51,55 +46,24 @@ function init() {
       new ol.layer.Tile({
         source: new ol.source.OSM(),
       }),
-      ],
-      view: new ol.View({
-      center: pos1,
+      layer
+    ],
+    view: new ol.View({
+      center: posRossi,
       zoom: 17,
     }),
-    overlay: overlay,
+    overlays: [overlay],
   });
-  var layer = new ol.layer.Vector({
-    source: new ol.source.Vector({
-        features: [featureRossi, featureRotatoria]
-    })
+
+  map.on("click", function(evt) {
+    const features = map.getFeaturesAtPixel(evt.pixel);
+    if (features.length > 0) {
+      const feature = features[features.length - 1];
+      popup.innerHTML = feature.get("name");
+      overlay.setPosition(feature.getGeometry().flatCoordinates);
+    }
+    else {
+      overlay.setPosition(undefined);
+    }
   });
-  map.addLayer(layer);
-
-  const popup = new ol.Overlay({
-    element: container,
-    positioning: "bottom",
-    stopEvent: false,
-  })
-  map.addOverlay(popup);
-
-  let popover;
-  function disposePopover(){
-    if(popover){
-      popover.dispose()
-      popover = undefined;
-    }
-  }
-
-  map.on("click", function(evt){
-    const feature = map.forEachFeatureAtPixel(evt.pixel, function(feature){
-      if(feature.length > 0){
-        return feature[feature.length - 1];
-      }
-      return feature;
-    });
-    disposePopover();
-    if(!feature){
-      popup.setPosition(undefined);
-      return;
-    }
-    popup.setPosition(evt.coordinate);
-    popover = new bootstrap.Popover(content, {
-      placement: "bottom",
-      html: true,
-      content: feature.get("name"),
-    });
-    popover.show();
-  })
-
-  map.on("movestart", disposePopover);
 }
